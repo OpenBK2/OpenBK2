eb9fb5df289ed967a2e0271ae990011501574a22
Marshal666 <adamklr720@gmail.com>
2026-06-20 15:13:18 +0200

    fix graveyard unordererd_map iter determinism

--- Versions/Temporary/Engine/Sources/AILogic/Graveyard.cpp
+++ Versions/Temporary/Engine/Sources/AILogic/Graveyard.cpp
@@ -254,13 +254,24 @@ void CGraveyard::DelKilledUnitsFromBridge( const SRect &bridgeRect )
 void CGraveyard::CheckSoonBeDead()
 {
 	// The quection is: to be or not to be?...
-	std::list<CAIUnit*> deadObjs;
+	std::vector<CAIUnit*> deadObjs{16};
+
+	std::vector<std::pair<CObj<CAIUnit>, float>> soonToDie{16};
 	for ( UpdateObjSet::iterator iter = soonBeDead.begin(); iter != soonBeDead.end(); ++iter )
+		soonToDie.push_back(iter->second);
+	
+	std::sort(soonToDie.begin(), soonToDie.end(), [](std::pair<CObj<CAIUnit>, float> u1, std::pair<CObj<CAIUnit>, float> u2)
 	{
-		CAIUnit *pUnit = iter->second.first;
+		return u1.first->GetUniqueId() > u2.first->GetUniqueID();
+	});
+
+	for ( size_t i = 0; i < soonToDie.size(); i++ )
+	{
+		auto& iter = soonToDie[i];
+		CAIUnit *pUnit = iter.first;
 		if ( pUnit->GetTimeOfDeath() <= curTime )
 		{
-			const float fDamage = iter->second.second;
+			const float fDamage = iter.second;
 			NDb::EAnimationType eDeathAnimType = NDb::ANIMATION_DEATH;
 			const int nFatality = pUnit->ChooseFatality( fDamage, &eDeathAnimType );
 			const bool bPutMud = !GetTerrain()->IsBridge( pUnit->GetCenterTile() );
@@ -283,7 +294,7 @@ void CGraveyard::CheckSoonBeDead()
 		}
 	}
 
-	for ( std::list<CAIUnit*>::iterator iter = deadObjs.begin(); iter != deadObjs.end(); ++iter )
+	for ( std::vector<CAIUnit*>::iterator iter = deadObjs.begin(); iter != deadObjs.end(); ++iter )
 		soonBeDead.erase( (*iter)->GetUniqueId() );
 }
 
