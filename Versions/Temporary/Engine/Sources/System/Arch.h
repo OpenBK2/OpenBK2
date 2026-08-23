f5ba21a728bf982e102cc95bedac3d98de8c3635
SSE4 <tomskside@gmail.com>
2026-08-23 10:06:53 +0700

    Indent Arch.h with the repo's preprocessor style
    Arch.h was the only new file indented with spaces: the bodies of its #if
    blocks sat four spaces in, while the rest of the engine puts preprocessor
    directives at column 0 and uses tabs for code. Move the directives flush
    left, fold the HAS_SSE2 condition onto one line so the backslash
    continuation and its space-indented tail go away, and add the missing
    newline at end of file.
    
    No macro definitions or conditions change.

--- Versions/Temporary/Engine/Sources/System/Arch.h
+++ Versions/Temporary/Engine/Sources/System/Arch.h
@@ -1,24 +1,23 @@
 #pragma once
 
 // Simple static detection of possible vector SIMD instruction sets, given by the compiler
-#if defined(__SSE2__) || defined(_M_X64) || \
-    (defined(_M_IX86_FP) && _M_IX86_FP >= 2)
-    #define HAS_SSE2 1
-    #pragma message("SSE2 is supported!")
+#if defined(__SSE2__) || defined(_M_X64) || ( defined(_M_IX86_FP) && _M_IX86_FP >= 2 )
+#define HAS_SSE2 1
+#pragma message("SSE2 is supported!")
 #else
-    #define HAS_SSE2 0
+#define HAS_SSE2 0
 #endif
 
 #if defined(__AVX2__)
-    #pragma message("AVX2 is supported!")
-    #define HAS_AVX2 1
+#pragma message("AVX2 is supported!")
+#define HAS_AVX2 1
 #else
-    #define HAS_AVX2 0
+#define HAS_AVX2 0
 #endif
 
 #if defined(__AVX512F__)
-    #pragma message("AVX512F is supported!")
-    #define HAS_AVX512F 1
+#pragma message("AVX512F is supported!")
+#define HAS_AVX512F 1
 #else
-    #define HAS_AVX512F 0
+#define HAS_AVX512F 0
 #endif
